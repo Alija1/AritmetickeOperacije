4cb7e44bba2967532ab23542d964d18ffae2ff0e
Alija1 <58340674+Alija1@users.noreply.github.com>
2020-06-04 19:28:17 +0200

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,20 @@
+let a = prompt("Please enter number A:");
+let c = prompt("Please enter an arithmetic operator:");
+let b = prompt("Please enter number B:");
+let result;
+
+if (c = "+"){result = parseInt(a) + parseInt(b);
+alert("The result is: " + result);
+}
+
+else if (c = "-"){result = parseInt(a) - parseInt(b);
+    alert("The result is: " + result);
+}
+
+else if (c = "*"){result = parseInt(a) * parseInt(b);
+    alert("The result is: " + result);
+}
+
+else if (c = "/"){result = parseInt(a) / parseInt(b);
+    alert("The result is: " + result);
+}
